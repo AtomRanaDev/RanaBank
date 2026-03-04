9ca82903f84459e1db909d2bb6dfc42fa404801d
Anurag Rana <atomranadev@gmail.com>
2026-03-04 14:20:39 +0530

    Fix signup to use BankDAO connection

--- src/main/java/com/bankmanagement/web/SignupThreeServlet.java
+++ src/main/java/com/bankmanagement/web/SignupThreeServlet.java
@@ -6,27 +6,48 @@ import java.io.*;
 import java.sql.*;
 
 public class SignupThreeServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String accountType = request.getParameter("accountType");
-        String cardNumber = request.getParameter("cardNumber");
-        String pin = request.getParameter("pin");
         String facility = request.getParameter("facility");
 
+        long card = (long)(Math.random() * 9000000000000000L) + 1000000000000000L;
+        String cardNumber = String.valueOf(card);
+
+        int pinNum = (int)(Math.random() * 9000) + 1000;
+        String pin = String.valueOf(pinNum);
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/banksystem", "root", "131486");
-            PreparedStatement ps = con.prepareStatement("INSERT INTO signupthree (account_Type, card_number, pin, facility) VALUES (?,?,?,?)");
+
+            Connection con = BankDAO.getConnection();
+
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO signupthree(account_Type,card_number,pin,facility,balance) VALUES(?,?,?,?,?)");
+
             ps.setString(1, accountType);
             ps.setString(2, cardNumber);
             ps.setString(3, pin);
             ps.setString(4, facility);
+            ps.setDouble(5, 0);
 
             ps.executeUpdate();
+
             con.close();
 
-            response.sendRedirect("login.jsp");
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            out.println("<h2 style='color:green;'>Account Created Successfully</h2>");
+            out.println("<p><b>Card Number:</b> " + cardNumber + "</p>");
+            out.println("<p><b>PIN:</b> " + pin + "</p>");
+            out.println("<a href='login.jsp'>Go to Login</a>");
+
         } catch (Exception e) {
+
+            e.printStackTrace();
             response.getWriter().println("Database Error: " + e.getMessage());
+
         }
     }
 }
